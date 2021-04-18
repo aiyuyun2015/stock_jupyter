ea5d70167658a72aac1cfc5f67800d4609a6adaa
aiyuyun2015 <alvin2009@foxmail.com>
2021-04-18 12:48:42 -0400

    add rundocker

---
+++ run_docker.sh
@@ -0,0 +1,1 @@
+docker run -it -p 7777:7777 -v "$PWD":/home/demo python_image
--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 docker build -t python_image -f Dockerfile .
-docker run -it -p 7777:7777 -v "$PWD":/home/demo python_image
 python3 -m venv env
 source env/bin/activate
 pip install dvc[all]
