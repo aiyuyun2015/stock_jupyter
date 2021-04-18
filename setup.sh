f0c7cc487104e273b26d9cd8a4c9690b1d34b2c6
aiyuyun2015 <alvin2009@foxmail.com>
2021-04-18 12:46:44 -0400

    add config back, update setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 docker build -t python_image -f Dockerfile .
 docker run -it -p 7777:7777 -v "$PWD":/home/demo python_image
-python3 -m venv
+python3 -m venv env
 source env/bin/activate
 pip install dvc[all]
 
